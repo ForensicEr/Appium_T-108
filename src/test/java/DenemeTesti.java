cd5d0aabb75c1da21a550986a661b5841d08878d
Ersin <forentom@gmail.com>
2023-04-20 10:18:03 +0200

    commit

---
+++ src/test/java/DenemeTesti.java
@@ -0,0 +1,33 @@
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+public class DenemeTesti {
+
+    AndroidDriver<MobileElement> driver;
+    final String cihazAdi="PIXEL";
+    final String platformIsmi="Android";
+    final String version="10.0";
+    final String automation="UiAutomator2";
+
+
+    @Test
+    public void deneme1() throws MalformedURLException {
+
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,cihazAdi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME,platformIsmi);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,version);
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
+        capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Ersin\\Desktop\\gitHubTeam108\\Appium_T-108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
+
+        driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+}
