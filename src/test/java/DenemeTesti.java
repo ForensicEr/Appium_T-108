f4c722c588b3fa67848d7468d17e38aef8015dc3
Ersin <forentom@gmail.com>
2023-04-20 11:46:22 +0200

    commot

--- src/test/java/DenemeTesti.java
+++ src/test/java/DenemeTesti.java
@@ -1,7 +1,11 @@
+import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -10,14 +14,15 @@ import java.util.concurrent.TimeUnit;
 
 public class DenemeTesti {
 
-    AndroidDriver<MobileElement> driver;
-    final String cihazAdi="PIXEL";
+    AndroidDriver<AndroidElement> driver;
+    // AppiumDriver<MobileElement> appiumDriver; // hem Android'de hem de Ios'dacalisir
+    final String cihazAdi="PIXEL2";
     final String platformIsmi="Android";
     final String version="10.0";
     final String automation="UiAutomator2";
 
 
-    @Test
+    @BeforeTest
     public void deneme1() throws MalformedURLException {
 
         DesiredCapabilities capabilities= new DesiredCapabilities();
@@ -27,7 +32,21 @@ public class DenemeTesti {
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,automation);
         capabilities.setCapability(MobileCapabilityType.APP,"C:\\Users\\Ersin\\Desktop\\gitHubTeam108\\Appium_T-108\\Apps\\Calculator_8.4 (503542421)_Apkpure.apk");
 
-        driver=new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        driver=new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
+    @Test
+    public void carpma(){
+        driver.findElementByAccessibilityId("8").click();
+        driver.findElementByAccessibilityId("0").click();
+        driver.findElementByAccessibilityId("multiply").click();
+        driver.findElementByAccessibilityId("2").click();
+        driver.findElementByAccessibilityId("0").click();
+        String result= driver.findElementById("com.google.android.calculator:id/result_preview").getText();
+        System.out.println(result);
+
+        Assert.assertEquals(Integer.parseInt(result),1600);
+    }
+
+
 }
