ac1a6b5bc372831d34212eea298ef4f5b5328246
Ersin <forentom@gmail.com>
2023-04-24 13:42:16 +0200

    commit

---
+++ src/test/java/Pages/KiwiPage.java
@@ -0,0 +1,33 @@
+package Pages;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.time.Duration;
+
+public class KiwiPage {
+
+    public KiwiPage (){
+        PageFactory.initElements(Driver.getAndroidDriver(),this);
+    }
+    @FindBy (xpath = "//*[@text='Continue as a guest']")
+    public WebElement asAGuest;
+
+    public void ucButtonTiklama(int baslangic, int bitis, int xCoordinat, int yCoordinat, int wait){
+        TouchAction action=new TouchAction<>(Driver.getAndroidDriver());
+        for (int i = baslangic; i <bitis ; i++) {
+            action.press(PointOption.point(xCoordinat,yCoordinat)).
+                    waitAction(WaitOptions.waitOptions(Duration.ofMillis(wait)))
+                    .release().
+                    perform();
+
+        }
+    }
+
+
+}
